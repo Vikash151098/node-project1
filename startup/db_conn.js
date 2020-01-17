9be6cddafed73c6f963e8ad5ea48f0dc3bdced07
Vikash151098 <vkumar0961@gmail.com>
2020-01-17 12:46:01 +0530

    changing conn str

--- startup/db_conn.js
+++ startup/db_conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 module.exports=function(){
-const connStr='mongodb://project1:12345@cluster0-shard-00-00-lhvtt.azure.mongodb.net:27017,cluster0-shard-00-01-lhvtt.azure.mongodb.net:27017,cluster0-shard-00-02-lhvtt.azure.mongodb.net:27017/node-project1?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+const connStr='mongodb://vidlyuser:12345@cluster0-shard-00-00-onnex.mongodb.net:27017,cluster0-shard-00-01-onnex.mongodb.net:27017,cluster0-shard-00-02-onnex.mongodb.net:27017/login?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 mongoose.connect(connStr)
     .then(() => console.log('connecting to MongoDB...'))
     .catch(err => console.log('could not connect MongoDB..', err));
