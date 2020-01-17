929fd6a850edbe834762e7fb9dfa0a52a12c1e74
Vikash151098 <vkumar0961@gmail.com>
2020-01-17 12:40:25 +0530

    add connStr

--- startup/db_conn.js
+++ startup/db_conn.js
@@ -1,8 +1,7 @@
 const mongoose=require('mongoose');
 module.exports=function(){
 const connStr='mongodb://project1:12345@cluster0-shard-00-00-lhvtt.azure.mongodb.net:27017,cluster0-shard-00-01-lhvtt.azure.mongodb.net:27017,cluster0-shard-00-02-lhvtt.azure.mongodb.net:27017/node-project1?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-mongoose.connect(connStr,{
-    useNewUrlParser:true })
+mongoose.connect(connStr)
     .then(() => console.log('connecting to MongoDB...'))
     .catch(err => console.log('could not connect MongoDB..', err));
 }
